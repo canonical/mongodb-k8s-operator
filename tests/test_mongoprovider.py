d2efe277f293c68ad1f3163e72da880e943c0389
Balbir Thomas <balbir.thomas@canonical.com>
2021-03-26 17:13:50 +0000

    More unit tests
    This commit adds many more unit tests to get good test coverage.

---
+++ tests/test_mongoprovider.py
@@ -0,0 +1,171 @@
+# Copyright 2021 Canonical Ltd
+# See LICENSE file for licensing details.
+
+import json
+import unittest
+
+from ops.testing import Harness
+from ops.charm import CharmBase
+from mongoprovider import MongoProvider
+
+# Used to populate Fake MongoDB metadata.yaml
+METADATA = {
+    'relation_name': 'database',
+    'interface_name': 'mongodb'
+}
+
+# Template for Fake MongoDB metadata.yaml
+PROVIDER_META = '''
+name: fake-mongodb-charm
+provides:
+  {relation_name}:
+    interface: {interface_name}
+'''
+
+# Used to populate Fake MongoDB config.yaml
+CONFIG = {
+    'relation_name': METADATA['relation_name'],
+    'is_joined': True,
+    'db_version': '4.1.1',
+    'standalone_uri': 'mongodb://localhost:12701',
+    'replica_set_name': 'rs0',
+    'replica_set_uri': 'mongodb://rs0:12701',
+    'available_dbs': json.dumps([])
+}
+
+# Template for Fake MongoDB charm config.yaml
+CONFIG_YAML = '''
+options:
+  relation_name:
+    type: string
+    description: 'Fake Relation name used for testing'
+    default: {relation_name}
+  is_joined:
+    type: boolean
+    description: 'Does charm have peers'
+    default: {is_joined}
+  db_version:
+    type: string
+    description: 'Fake MongoDB version used for testing'
+    default: {db_version}
+  standalone_uri:
+    type: string
+    description: 'Fake URI of single MongoDB instance'
+    default: {standalone_uri}
+  replica_set_name:
+    type: string
+    description: 'Name of fake replica set'
+    default: {replica_set_name}
+  replica_set_uri:
+    type: string
+    description: 'Fake URI of replicated MongoDB'
+    default: {replica_set_uri}
+  available_dbs:
+    type: string
+    description: 'JSON list of availabe databases'
+    default: {available_dbs}
+'''
+
+
+class Mongo:
+    """Fake mongoserver.MongoDB class"""
+    def __init__(self, charm):
+        self.charm = charm
+
+    @property
+    def databases(self):
+        """Return list fake list of available databases"""
+        dbs = self.charm.model.config['available_dbs']
+        return dbs
+
+
+class MongoDBCharm(CharmBase):
+    """A Fake MongoDB charm used for unit testing MongoProvider"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+        self.mongo = Mongo(self)
+        self.provider = MongoProvider(self,
+                                      self.model.config['relation_name'],
+                                      self.provides)
+
+    @property
+    def provides(self):
+        """Fake provides injects information using model config unlike a real charm"""
+        db_info = {
+            'replicated': self.model.config['is_joined'],
+            'replica_set_name': self.model.config['replica_set_name'],
+            'standalone_uri': self.model.config['standalone_uri'],
+            'replica_set_uri': self.model.config['replica_set_uri']
+        }
+        provided = {
+            "provides": {"mongodb": self.model.config['db_version']},
+            "config": db_info
+        }
+        return provided
+
+
+class TestMongoProvider(unittest.TestCase):
+    def setup_harness(self, config, meta):
+        config_yaml = CONFIG_YAML.format(**config)
+        meta_yaml = PROVIDER_META.format(**meta)
+        self.harness = Harness(MongoDBCharm, meta=meta_yaml, config=config_yaml)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.set_leader(True)
+        self.harness.begin()
+
+    def test_databases_are_created_when_requested(self):
+        config = CONFIG.copy()
+        meta = METADATA.copy()
+        self.setup_harness(config, meta)
+
+        requested_database = ['mydb']
+        json_request = json.dumps(requested_database)
+        consumer_data = {'databases': json_request}
+
+        rel_id = self.harness.add_relation('database', 'consumer')
+        data = self.harness.get_relation_data(rel_id, self.harness.model.app.name)
+        self.assertDictEqual(data, {})
+        self.harness.add_relation_unit(rel_id, 'consumer/0')
+        self.harness.update_relation_data(rel_id, 'consumer', consumer_data)
+        data = self.harness.get_relation_data(rel_id, self.harness.model.app.name)
+        databases = json.loads(data['databases'])
+        self.assertListEqual(databases, requested_database)
+
+    def test_databases_are_not_created_without_a_new_request(self):
+        config = CONFIG.copy()
+        config['available_dbs'] = json.dumps(['mydb1'])
+        meta = METADATA.copy()
+        self.setup_harness(config, meta)
+
+        requested_database = ['mydb1']
+        json_request = json.dumps(requested_database)
+        consumer_data = {'databases': json_request}
+
+        rel_id = self.harness.add_relation('database', 'consumer')
+        data = self.harness.get_relation_data(rel_id, self.harness.model.app.name)
+        self.assertDictEqual(data, {})
+        self.harness.add_relation_unit(rel_id, 'consumer/0')
+        self.harness.update_relation_data(rel_id, 'consumer', consumer_data)
+        data = self.harness.get_relation_data(rel_id, self.harness.model.app.name)
+        self.assertDictEqual(data.get('databases', {}), {})
+
+    def test_databases_are_created_only_by_leader_unit(self):
+        config = CONFIG.copy()
+        meta = METADATA.copy()
+        self.setup_harness(config, meta)
+        self.harness.set_leader(False)
+
+        requested_database = ['mydb']
+        json_request = json.dumps(requested_database)
+        consumer_data = {'databases': json_request}
+
+        rel_id = self.harness.add_relation('database', 'consumer')
+        data = self.harness.get_relation_data(rel_id, self.harness.model.app.name)
+        self.assertDictEqual(data, {})
+        self.harness.add_relation_unit(rel_id, 'consumer/0')
+        self.harness.update_relation_data(rel_id, 'consumer', consumer_data)
+        data = self.harness.get_relation_data(rel_id, self.harness.model.app.name)
+        dbs = data.get('databases', '[]')
+        databases = json.loads(dbs)
+        self.assertListEqual(databases, [])
---
+++ tests/test_mongoserver.py
@@ -0,0 +1,111 @@
+# Copyright 2021 Canonical Ltd
+# See LICENSE file for licensing details.
+
+import unittest
+
+from unittest.mock import patch
+from pymongo import MongoClient
+from mongoserver import MongoDB
+
+MONGO_CONFIG = {
+    'app_name': 'mongodb',
+    'replica_set_name': 'rs0',
+    'num_peers': 2,
+    'port': 27017,
+    'root_password': 'password'
+}
+
+
+class TestMongoServer(unittest.TestCase):
+
+    def test_get_client_returns_mongo_client_instance(self):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        client = mongo.get_client()
+        self.assertIsInstance(client, MongoClient)
+
+    def test_get_replica_set_client_returns_mongo_client_instance(self):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        client = mongo.get_replica_set_client()
+        self.assertIsInstance(client, MongoClient)
+
+    @patch('pymongo.MongoClient.server_info')
+    def test_mongo_is_ready_when_server_info_is_available(self, server_info):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        server_info.return_value = {"info": "some info"}
+        ready = mongo.is_ready()
+        self.assertEqual(ready, True)
+
+    @patch('pymongo.MongoClient', 'server_info', 'ServerSelectionTimeoutError')
+    def test_mongo_is_not_ready_when_server_info_is_not_available(self):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        ready = mongo.is_ready()
+        self.assertEqual(ready, False)
+
+    @patch('mongoserver.MongoDB.get_replica_set_client')
+    @patch('pymongo.MongoClient')
+    def test_reconfiguring_replica_invokes_admin_command(self, mock_client, mock_get):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        mock_get.return_value = mock_client
+
+        hosts = {}
+        for i in range(config['num_peers']):
+            hosts['i'] = "host{}".format(i)
+
+        mongo.reconfigure_replica_set(hosts)
+        mock_client.admin.command.assert_called()
+        command, _ = mock_client.admin.command.call_args
+        self.assertEqual("replSetReconfig", command[0])
+
+    @patch('mongoserver.MongoDB.get_client')
+    @patch('pymongo.MongoClient')
+    def test_initializing_replica_invokes_admin_command(self, mock_client, mock_get):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        mock_get.return_value = mock_client
+
+        hosts = {}
+        for i in range(config['num_peers']):
+            hosts['i'] = "host{}".format(i)
+
+        mongo.initialize_replica_set(hosts)
+        mock_client.admin.command.assert_called()
+        command, _ = mock_client.admin.command.call_args
+        self.assertEqual("replSetInitiate", command[0])
+
+    def test_replica_set_uri_contains_correct_number_of_hosts(self):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        host_list = mongo.replica_set_uri.split(',')
+        self.assertEqual(len(host_list), config['num_peers'])
+
+    def test_replica_set_uri_has_correct_root_credentials(self):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        prefix, _ = mongo.replica_set_uri.split('@')
+        _, user, password = prefix.split(':')
+        user = user.lstrip("/")
+        self.assertEqual("root", user)
+        self.assertEqual(password, config['root_password'])
+
+    def test_standalone_uri_has_correct_root_credentials(self):
+        config = MONGO_CONFIG.copy()
+        mongo = MongoDB(config)
+
+        prefix, _ = mongo.standalone_uri.split('@')
+        _, user, password = prefix.split(':')
+        user = user.lstrip("/")
+        self.assertEqual("root", user)
+        self.assertEqual(password, config['root_password'])
