cce91d2ec853601abe0bfef1182ac630f606f5c9
Balbir Thomas <balbir.thomas@canonical.com>
2021-04-16 10:50:39 +0100

    Updated MongoDB charm lib
    The charm library now holds the charm responsible for providing
    relation id to methods that require it, where there is more
    than one relation of the same name. A few utility methods
    have also been provided that make this easy.

--- lib/charms/mongodb/v1/mongodb.py
+++ lib/charms/mongodb/v1/mongodb.py
@@ -1,11 +1,9 @@
 import json
 import uuid
-import logging
 from ops.relation import Consumer
 
 LIBAPI = 1
 LIBPATCH = 0
-logger = logging.getLogger(__name__)
 
 
 class MongoConsumer(Consumer):
@@ -14,40 +12,85 @@ class MongoConsumer(Consumer):
         self.charm = charm
         self.relation_name = name
 
-    def databases(self):
+    def provider_ids(self):
+        """Return relation IDs of all database providers
+        """
+        id_list = []
+        for rel in self.charm.model.relations[self.relation_name]:
+            id_list.append(rel.id)
+        return id_list
+
+    def credentials(self, rel_id=None):
+        """Get authentication credentials for particular provider
+
+        Args:
+            rel_id: id of relation for which credentials are required.
+
+        Returns:
+            dictionary containing "username" and "password" credentials
+            if available, otherwise an empty dictionary is returned.
+        """
+
+        rel = self.framework.model.get_relation(self.relation_name, rel_id)
+        relation_data = rel.data[rel.app]
+        username = relation_data.get('username')
+        password = relation_data.get('password')
+        replica_set_uri = relation_data.get('replica_set_uri')
+        standalone_uri = relation_data.get('standalone_uri')
+        replicated = relation_data.get('replicated')
+
+        credentials = {}
+        if username and password:
+            credentials = {"username": username,
+                           "password": password,
+                           "replica_set_uri": replica_set_uri,
+                           "standalone_uri": standalone_uri,
+                           "replicated": replicated}
+        return credentials
+
+    def databases(self, rel_id=None):
         """List of currently available databases
+
+        Args:
+            rel_id: id of relation for which database list is required.
+                This is optional in single relation mode but if it is
+                not provided in multi mode then TooManyRelatedAppsError
+                exception is raised.
+
+        Raises:
+            TooManyRelatedAppsError if relation id is not provided and
+            multiple relation of the same name are present
 
         Returns:
             list: list of database names
         """
-        rel_id = super().stored.relation_id
-        if rel_id:
-            rel = self.framework.model.get_relation(self.relation_name, rel_id)
-        else:
-            rel = self.framework.model.get_relation(self.relation_name)
-
+        rel = self.framework.model.get_relation(self.relation_name, rel_id)
         relation_data = rel.data[rel.app]
         dbs = relation_data.get('databases')
         databases = json.loads(dbs) if dbs else []
 
         return databases
 
-    def new_database(self):
+    def new_database(self, rel_id=None):
         """Request creation of an additional database
 
+        Args:
+            rel_id: id of relation for which database list is required.
+                This is optional in single relation mode but if it is
+                not provided in multi mode then TooManyRelatedAppsError
+                exception is raised.
+
+        Raises:
+            TooManyRelatedAppsError if relation id is not provided and
+            multiple relation of the same name are present
         """
         if not self.charm.unit.is_leader():
             return
 
-        rel_id = super().stored.relation_id
-        if rel_id:
-            rel = self.framework.model.get_relation(self.relation_name, rel_id)
-        else:
-            rel = self.framework.model.get_relation(self.relation_name)
+        rel = self.framework.model.get_relation(self.relation_name, rel_id)
 
         id = uuid.uuid4()
         db_name = "db-{}-{}".format(rel.id, id)
-        logger.debug("CLIENT REQUEST {}".format(db_name))
         rel_data = rel.data[self.charm.app]
         dbs = rel_data.get('databases')
         dbs = json.loads(dbs) if dbs else []
