a6642bbe4ec537c10e05f3803d0fbb15a277feb7
Balbir Thomas <balbir.thomas@canonical.com>
2021-09-14 10:52:35 +0100

    Updated library patch level
    This commit updates the library patch level since dependence
    on ops.relation has now been removed.

--- lib/charms/mongodb_k8s/v0/mongodb.py
+++ lib/charms/mongodb_k8s/v0/mongodb.py
@@ -10,7 +10,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 2
 
 
 class MongoConsumer(Object):
