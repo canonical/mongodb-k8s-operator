fee689f79413c0e8d034782436d5f1b24f74b04a
Mykola Marzhan <303592+delgod@users.noreply.github.com>
2022-09-07 11:57:13 +0200

    get certificates from TLS relation v1

--- lib/charms/mongodb_libs/v0/mongodb_tls.py
+++ lib/charms/mongodb_libs/v0/mongodb_tls.py
@@ -15,8 +15,11 @@ import socket
 from cryptography import x509
 from cryptography.x509.extensions import ExtensionType
 from ops.framework import Object
-from ops.charm import ActionEvent
+from ops.charm import ActionEvent, RelationJoinedEvent, RelationBrokenEvent
 from charms.tls_certificates_interface.v1.tls_certificates import (
+    CertificateAvailableEvent,
+    CertificateExpiringEvent,
+    TLSCertificatesRequiresV1,
     generate_csr,
     generate_private_key,
 )
@@ -44,7 +47,12 @@ class MongoDBTLS(Object):
         super().__init__(charm, "client-relations")
         self.charm = charm
         self.peer_relation = peer_relation
+        self.certs = TLSCertificatesRequiresV1(self.charm, TLS_RELATION)
         self.framework.observe(self.charm.on.set_tls_private_key_action, self._on_set_tls_private_key)
+        self.framework.observe(self.charm.on[TLS_RELATION].relation_joined, self._on_tls_relation_joined)
+        self.framework.observe(self.charm.on[TLS_RELATION].relation_broken, self._on_tls_relation_broken)
+        self.framework.observe(self.certs.on.certificate_available, self._on_certificate_available)
+        self.framework.observe(self.certs.on.certificate_expiring, self._on_certificate_expiring)
 
     def _on_set_tls_private_key(self, event: ActionEvent) -> None:
         """Set the TLS private key, which will be used for requesting the certificate."""
@@ -75,6 +83,9 @@ class MongoDBTLS(Object):
         self.charm.set_secret(scope, "key", key.decode("utf-8"))
         self.charm.set_secret(scope, "csr", csr.decode("utf-8"))
 
+        if self.charm.model.get_relation(TLS_RELATION):
+            self.certs.request_certificate_creation(certificate_signing_request=csr)
+
     @staticmethod
     def _parse_tls_file(raw_content: str) -> bytes:
         """Parse TLS files from both plain text or base64 format."""
@@ -85,6 +96,87 @@ class MongoDBTLS(Object):
                 raw_content,
             ).encode("utf-8")
         return base64.b64decode(raw_content)
+
+    def _on_tls_relation_joined(self, _: RelationJoinedEvent) -> None:
+        """Request certificate when TLS relation joined."""
+        if self.charm.unit.is_leader():
+            self._request_certificate("app", None)
+
+        self._request_certificate("unit", None)
+
+    def _on_tls_relation_broken(self, event: RelationBrokenEvent) -> None:
+        """Disable TLS when TLS relation broken."""
+        self.charm.set_secret("unit", "ca", None)
+        self.charm.set_secret("unit", "cert", None)
+        self.charm.set_secret("unit", "chain", None)
+        if self.charm.unit.is_leader():
+            self.charm.set_secret("app", "ca", None)
+            self.charm.set_secret("app", "cert", None)
+            self.charm.set_secret("app", "chain", None)
+        if self.charm.get_secret("app", "cert"):
+            logger.debug(
+                "Defer till the leader delete the internal TLS certificate to avoid second restart."
+            )
+            event.defer()
+            return
+        self.charm.on_mongod_pebble_ready(event)
+
+    def _on_certificate_available(self, event: CertificateAvailableEvent) -> None:
+        """Enable TLS when TLS certificate available."""
+        if event.certificate_signing_request.rstrip() == self.charm.get_secret("unit", "csr").rstrip():
+            logger.debug("The external TLS certificate available.")
+            scope = "unit"  # external crs
+        elif event.certificate_signing_request.rstrip() == self.charm.get_secret("app", "csr").rstrip():
+            logger.debug("The internal TLS certificate available.")
+            if not self.charm.unit.is_leader():
+                return
+            scope = "app"  # internal crs
+        else:
+            logger.error("An unknown certificate available.")
+            return
+
+        old_cert = self.charm.get_secret(scope, "cert")
+        renewal = old_cert and old_cert != event.certificate
+        self.charm.set_secret(scope, "chain", event.chain)
+        self.charm.set_secret(scope, "cert", event.certificate)
+        self.charm.set_secret(scope, "ca", event.ca)
+
+        if renewal:
+            self.charm.unit.get_container("mongod").stop("mongod")
+        elif not self.charm.get_secret("app", "cert") or not self.charm.get_secret("unit", "cert"):
+            logger.debug(
+                "Defer till both internal and external TLS certificates available to avoid second restart."
+            )
+            event.defer()
+            return
+
+        self.charm.on_mongod_pebble_ready(event)
+
+    def _on_certificate_expiring(self, event: CertificateExpiringEvent) -> None:
+        """Request the new certificate when old certificate is expiring."""
+        if event.certificate.rstrip() == self.charm.get_secret("unit", "cert").rstrip():
+            logger.debug("The external TLS certificate expiring.")
+            scope = "unit"  # external cert
+        elif event.certificate.rstrip() == self.charm.get_secret("app", "cert").rstrip():
+            logger.debug("The internal TLS certificate expiring.")
+            scope = "app"  # internal cert
+        else:
+            logger.error("An unknown certificate expiring.")
+            return
+
+        key = self.charm.get_secret(scope, "key").encode("utf-8")
+        old_csr = self.charm.get_secret(scope, "csr").encode("utf-8")
+        new_csr = generate_csr(
+            private_key=key,
+            subject=self.charm.get_hostname_by_unit(self.charm.unit.name),
+            organization=self.charm.app.name,
+            sans=self._get_sans(),
+        )
+        self.certs.request_certificate_renewal(
+            old_certificate_signing_request=old_csr,
+            new_certificate_signing_request=new_csr,
+        )
+        self.charm.set_secret(scope, "csr", new_csr.decode("utf-8"))
 
     def _get_sans(self) -> List[str]:
         """Create a list of DNS names for a MongoDB unit.
