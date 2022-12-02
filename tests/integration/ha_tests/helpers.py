95c6772beb1c1490b0a1f7cfb04d5e3294c0c9e7
Dragomir Penev <dragomir.penev@canonical.com>
2022-12-02 16:00:16 +0200

    Linting

--- tests/integration/ha_tests/helpers.py
+++ tests/integration/ha_tests/helpers.py
@@ -1,7 +1,6 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import json
 import os
 import string
 import subprocess
@@ -9,7 +8,7 @@ import tempfile
 from asyncio import gather
 from datetime import datetime
 from pathlib import Path
-from typing import Dict, List, Optional
+from typing import List, Optional
 
 import ops
 import yaml
@@ -47,6 +46,7 @@ class ProcessRunningError(Exception):
 
 async def get_application_name(ops_test: OpsTest, application_name: str) -> str:
     """Returns the Application in the juju model that matches the provided application name.
+
     This enables us to retrieve the name of the deployed application in an existing model, while
      ignoring some test specific applications.
     Note: if multiple applications with the application name exist, the first one found will be
@@ -70,6 +70,7 @@ async def scale_application(
     ops_test: OpsTest, application_name: str, desired_count: int, wait: bool = True
 ) -> None:
     """Scale a given application to the desired unit count.
+
     Args:
         ops_test: The ops test framework
         application_name: The name of the application
@@ -98,6 +99,7 @@ async def relate_mongodb_and_application(
     ops_test: OpsTest, mongodb_application_name: str, application_name: str
 ) -> None:
     """Relates the mongodb and application charms.
+
     Args:
         ops_test: The ops test framework
         mongodb_application_name: The mongodb charm application name
@@ -127,6 +129,7 @@ async def deploy_and_scale_mongodb(
     charm_path: Optional[Path] = None,
 ) -> str:
     """Deploys and scales the mongodb application charm.
+
     Args:
         ops_test: The ops test framework
         check_for_existing_application: Whether to check for existing mongodb applications
@@ -173,6 +176,7 @@ async def deploy_and_scale_mongodb(
 
 async def deploy_and_scale_application(ops_test: OpsTest) -> str:
     """Deploys and scales the test application charm.
+
     Args:
         ops_test: The ops test framework
     """
@@ -208,6 +212,7 @@ async def deploy_and_scale_application(ops_test: OpsTest) -> str:
 
 def is_relation_joined(ops_test: OpsTest, endpoint_one: str, endpoint_two: str) -> bool:
     """Check if a relation is joined.
+
     Args:
         ops_test: The ops test object passed into every test case
         endpoint_one: The first endpoint of the relation
@@ -224,6 +229,7 @@ async def get_process_pid(
     ops_test: OpsTest, unit_name: str, container_name: str, process: str
 ) -> int:
     """Return the pid of a process running in a given unit.
+
     Args:
         ops_test: The ops test object passed into every test case
         unit_name: The name of the unit
@@ -260,6 +266,7 @@ async def send_signal_to_pod_container_process(
     ops_test: OpsTest, unit_name: str, container_name: str, process: str, signal_code: str
 ) -> None:
     """Send the specified signal to a pod container process.
+
     Args:
         ops_test: The ops test framework
         unit_name: The name of the unit to send signal to
@@ -321,6 +328,7 @@ async def count_primaries(ops_test: OpsTest) -> int:
 
 async def fetch_replica_set_members(ops_test: OpsTest) -> List[str]:
     """Fetches the hosts listed as replica set members in the MongoDB replica set configuration.
+
     Args:
         ops_test: reference to deployment.
     """
@@ -541,6 +549,7 @@ def retrieve_entries(client, db_name, collection_name, query_field):
 
 def deploy_chaos_mesh(namespace: str) -> None:
     """Deploy chaos mesh to the provided namespace.
+
     Args:
         namespace: The namespace to deploy chaos mesh to
     """
@@ -561,6 +570,7 @@ def deploy_chaos_mesh(namespace: str) -> None:
 
 def destroy_chaos_mesh(namespace: str) -> None:
     """Deploy chaos mesh to the provided namespace.
+
     Args:
         namespace: The namespace to deploy chaos mesh to
     """
