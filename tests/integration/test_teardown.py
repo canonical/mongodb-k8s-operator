c6f1feb225f56dadd2450806322e2190616c27a3
Dmitry Ratushnyy <dmitry.ratushnyy@canonical.com>
2023-06-06 10:08:04 +0000

    Address review comments

--- tests/integration/test_teardown.py
+++ tests/integration/test_teardown.py
@@ -8,7 +8,7 @@ import pytest
 from pytest_operator.plugin import OpsTest
 
 from .ha_tests.helpers import get_replica_set_primary as replica_set_primary
-from .helpers import METADATA
+from .helpers import METADATA, SERIES
 
 DATABASE_APP_NAME = "mongodb-k8s"
 MEDIAN_REELECTION_TIME = 12
@@ -30,7 +30,7 @@ async def test_build_and_deploy(ops_test: OpsTest):
         resources=resources,
         application_name=DATABASE_APP_NAME,
         num_units=1,
-        series="jammy",
+        series=SERIES,
     )
 
     # issuing dummy update_status just to trigger an event
