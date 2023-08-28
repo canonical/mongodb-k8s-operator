248f6a4fbb506f61f07b8db170a5260d9360c999
Mia Altieri <mgaltier200@gmail.com>
2023-08-28 07:38:07 +0000

    enrico comments

--- tests/integration/ha_tests/helpers.py
+++ tests/integration/ha_tests/helpers.py
@@ -840,8 +840,8 @@ def copy_file_into_pod(
     namespace: str,
     pod_name: str,
     container_name: str,
-    destination_path: str,
     source_path: str,
+    destination_path: str,
 ) -> None:
     """Copy file contents into pod.
 
@@ -850,8 +850,8 @@ def copy_file_into_pod(
         namespace: The namespace of the pod to copy files to
         pod_name: The name of the pod to copy files to
         container_name: The name of the pod container to copy files to
-        destination_path: The path to which the file should be copied over
-        source_path: The path of the file which needs to be copied over
+        source_path: The path to which the file should be copied over
+        destination_path: The path of the file which needs to be copied over
     """
     try:
         exec_command = ["tar", "xvf", "-", "-C", "/"]
@@ -871,7 +871,7 @@ def copy_file_into_pod(
 
         with tempfile.TemporaryFile() as tar_buffer:
             with tarfile.open(fileobj=tar_buffer, mode="w") as tar:
-                tar.add(source_path, destination_path)
+                tar.add(destination_path, source_path)
 
             tar_buffer.seek(0)
             commands = []
