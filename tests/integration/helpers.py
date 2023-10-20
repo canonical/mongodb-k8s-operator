8a41a5acc457ef34a39db5adc0427ea414723959
Dmitry Ratushnyy <dmitry.ratushnyy@canonical.com>
2023-10-20 06:29:34 +0000

    Release to 6/edge

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -38,6 +38,8 @@ TEST_DOCUMENTS = """[
 
 SERIES = "jammy"
 
+HELPER_MONGO_VERSION="6.0.11"
+
 logger = logging.getLogger(__name__)
 
 
@@ -121,9 +123,9 @@ async def run_mongo_op(
         mongo_uri = await mongodb_uri(ops_test)
 
     if stringify:
-        mongo_cmd = f"mongo --quiet --eval 'JSON.stringify({mongo_op})' {mongo_uri}{suffix}"
+        mongo_cmd = f"mongosh --quiet --eval 'JSON.stringify({mongo_op})' {mongo_uri}{suffix}"
     else:
-        mongo_cmd = f"mongo --quiet --eval '{mongo_op}' {mongo_uri}{suffix}"
+        mongo_cmd = f"mongosh --quiet --eval '{mongo_op}' {mongo_uri}{suffix}"
 
     logger.info("Running mongo command: %r", mongo_cmd)
     kubectl_cmd = (
@@ -137,7 +139,7 @@ async def run_mongo_op(
         "--command",
         f"--namespace={ops_test.model_name}",
         "mongo-test",
-        "--image=mongo:4.4",
+        f"--image=mongo:{HELPER_MONGO_VERSION}",
         "--",
         "sh",
         "-c",
