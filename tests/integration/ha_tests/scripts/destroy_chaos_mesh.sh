c20b057d89dfac09fda907673ca839ad79ae435c
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2024-04-23 00:20:01 +0200

    fix tox tasks

--- tests/integration/ha_tests/scripts/destroy_chaos_mesh.sh
+++ tests/integration/ha_tests/scripts/destroy_chaos_mesh.sh
@@ -10,41 +10,41 @@ fi
 
 destroy_chaos_mesh() {
     echo "deleting api-resources"
-    for i in $(kubectl api-resources | grep chaos-mesh | awk '{print $1}'); do timeout 30 kubectl delete ${i} --all --all-namespaces || :; done
+    for i in $(kubectl api-resources | grep chaos-mesh | awk '{print $1}'); do timeout 30 kubectl delete "${i}" --all --all-namespaces || :; done
     
-    if [ "$(kubectl -n ${chaos_mesh_ns} get mutatingwebhookconfiguration | grep 'choas-mesh-mutation' | wc -l)" = "1" ]; then
+    if [ "$(kubectl -n "${chaos_mesh_ns}" get mutatingwebhookconfiguration | grep -c 'choas-mesh-mutation')" = "1" ]; then
         echo "deleting chaos-mesh-mutation"    
-        timeout 30 kubectl -n ${chaos_mesh_ns} delete mutatingwebhookconfiguration chaos-mesh-mutation || :
+        timeout 30 kubectl -n "${chaos_mesh_ns}" delete mutatingwebhookconfiguration chaos-mesh-mutation || :
     fi
 
-    if [ "$(kubectl -n ${chaos_mesh_ns} get validatingwebhookconfiguration | grep 'chaos-mesh-validation-auth' | wc -l)" = "1" ]; then
+    if [ "$(kubectl -n "${chaos_mesh_ns}" get validatingwebhookconfiguration | grep -c 'chaos-mesh-validation-auth')" = "1" ]; then
         echo "deleting chaos-mesh-validation-auth"
-        timeout 30 kubectl -n ${chaos_mesh_ns} delete validatingwebhookconfiguration chaos-mesh-validation-auth || :
+        timeout 30 kubectl -n "${chaos_mesh_ns}" delete validatingwebhookconfiguration chaos-mesh-validation-auth || :
     fi
 
-    if [ "$(kubectl -n ${chaos_mesh_ns} get validatingwebhookconfiguration | grep 'chaos-mesh-validation' | wc -l)" = "1" ]; then
+    if [ "$(kubectl -n "${chaos_mesh_ns}" get validatingwebhookconfiguration | grep -c 'chaos-mesh-validation')" = "1" ]; then
         echo 'deleting chaos-mesh-validation'
-        timeout 30 kubectl -n ${chaos_mesh_ns} delete validatingwebhookconfiguration chaos-mesh-validation || :
+        timeout 30 kubectl -n "${chaos_mesh_ns}" delete validatingwebhookconfiguration chaos-mesh-validation || :
     fi
 
     if [ "$(kubectl get clusterrolebinding | grep 'chaos-mesh' | awk '{print $1}' | wc -l)" != "0" ]; then
         echo "deleting clusterrolebindings"
-        timeout 30 kubectl delete clusterrolebinding $(kubectl get clusterrolebinding | grep 'chaos-mesh' | awk '{print $1}') || :
+        timeout 30 kubectl delete clusterrolebinding "$(kubectl get clusterrolebinding | grep 'chaos-mesh' | awk '{print $1}')" || :
     fi
 
     if [ "$(kubectl get clusterrole | grep 'chaos-mesh' | awk '{print $1}' | wc -l)" != "0" ]; then
         echo "deleting clusterroles"
-        timeout 30 kubectl delete clusterrole $(kubectl get clusterrole | grep 'chaos-mesh' | awk '{print $1}') || :
+        timeout 30 kubectl delete clusterrole "$(kubectl get clusterrole | grep 'chaos-mesh' | awk '{print $1}')" || :
     fi
 
     if [ "$(kubectl get crd | grep 'chaos-mesh.org' | awk '{print $1}' | wc -l)" != "0" ]; then
         echo "deleting crds"
-        timeout 30 kubectl delete crd $(kubectl get crd | grep 'chaos-mesh.org' | awk '{print $1}') || :
+        timeout 30 kubectl delete crd "$(kubectl get crd | grep 'chaos-mesh.org' | awk '{print $1}')" || :
     fi
 
-    if [ -n "$chaos_mesh_ns" ] && [ "$(helm repo list --namespace $chaos_mesh_ns | grep 'chaos-mesh' | wc -l)" = "1" ]; then
+    if [ -n "${chaos_mesh_ns}" ] && [ "$(helm repo list --namespace "${chaos_mesh_ns}" | grep -c 'chaos-mesh')" = "1" ]; then
         echo "uninstalling chaos-mesh helm repo"
-        helm uninstall chaos-mesh --namespace ${chaos_mesh_ns} || :
+        helm uninstall chaos-mesh --namespace "${chaos_mesh_ns}" || :
     fi
 }
 
