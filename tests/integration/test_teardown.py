4b60d9377a484cb6d12235a32b7a5ece1d0a6448
Neha Oudin <neha@oudin.red>
2024-08-08 11:33:54 +0200

    fix: duplicated test mark

--- tests/integration/test_teardown.py
+++ tests/integration/test_teardown.py
@@ -17,7 +17,6 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
