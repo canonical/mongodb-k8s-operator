fc8fe2678d98369aea75e9bc4a11457eee7f23cb
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2024-11-12 22:15:03 +0100

    update libs

--- lib/charms/mongodb/v1/mongodb_backups.py
+++ lib/charms/mongodb/v1/mongodb_backups.py
@@ -41,7 +41,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 6
+LIBPATCH = 5
 
 logger = logging.getLogger(__name__)
 
