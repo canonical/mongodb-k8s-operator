90cff84ca40aac13dc22365e35da0b38ff23c6c1
Mia Altieri <mgaltier200@gmail.com>
2024-11-13 13:31:28 +0000

    wait for permissions to be resolved

--- src/charm.py
+++ src/charm.py
@@ -636,14 +636,16 @@ class MongoDBCharm(CharmBase):
         except FailedToUpdateFilesystem as err:
             raise ContainerNotReadyError from err
 
-        self._configure_layers(container)
-
-        # when a network cuts and the pod restarts - reconnect to the exporter
         try:
+            self._configure_layers(container)
+            # when a network cuts and the pod restarts - reconnect to the exporter and pbm
             self._connect_mongodb_exporter()
             self._connect_pbm_agent()
         except MissingSecretError as e:
             logger.error("Cannot connect mongodb exporter: %r", e)
+            raise ContainerNotReadyError
+        except ChangeError as e:
+            logger.error("Cannot configure container layers %r", e)
             raise ContainerNotReadyError
 
     # BEGIN: charm events
