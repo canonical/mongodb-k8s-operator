66d10305bac94bdf1791ecb0324a78b00dacbf84
Neha Oudin <17551419+Gu1nness@users.noreply.github.com>
2025-10-02 15:56:59 +0200

    feat: Terraform Modules (#427) (#430)
    * feat: Terraform Modules
    
    * fix: remove grafana agent
    
    * fix: Module
    
    * fix: consistency in naming
    
    * Update terraform/charm/replica_set/variables.tf
    
    
    
    * fix(module): 2 storages for mongodb k8s
    
    ---------
    
    Co-authored-by: Patricia Reinoso <patricia.reinoso@canonical.com>

---
+++ terraform/charm/sharded/variables.tf
@@ -0,0 +1,47 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+variable "config_server" {
+  description = "Config server app definition"
+  type = object({
+    app_name          = string
+    model             = string
+    config            = optional(map(string), { "role" : "config-server" })
+    channel           = optional(string, "6/stable")
+    base              = optional(string, "ubuntu@22.04")
+    revision          = optional(string, null)
+    units             = optional(number, 3)
+    constraints       = optional(string, "arch=amd64")
+    machines          = optional(set(string), null)
+    storage           = optional(map(string), {})
+    endpoint_bindings = optional(set(map(string)), [])
+  })
+
+  validation {
+    condition     = var.config_server.config["role"] == "config-server"
+    error_message = "Config option: 'role' must be set to 'config-server'."
+  }
+}
+
+variable "shards" {
+  description = "Shard apps"
+  type = list(object({
+    app_name          = string
+    model             = string
+    config            = optional(map(string), { "role" : "shard" })
+    channel           = optional(string, "6/stable")
+    base              = optional(string, "ubuntu@22.04")
+    revision          = optional(string, null)
+    units             = optional(number, 3)
+    constraints       = optional(string, "arch=amd64")
+    machines          = optional(set(string), null)
+    storage           = optional(map(string), {})
+    endpoint_bindings = optional(set(map(string)), [])
+  }))
+  default = []
+
+  validation {
+    condition     = alltrue([for shard in var.shards : (shard.config["role"] == "shard")])
+    error_message = "Config option: 'role' must be set to 'shard' in all shard objects."
+  }
+}
