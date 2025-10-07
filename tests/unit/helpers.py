dc119ef687355795634e531a923e140e825076da
Neha Oudin <17551419+Gu1nness@users.noreply.github.com>
2025-10-07 18:45:08 +0200

    patch: remove UTs from charm repo + bump DPW v35 (#435)
    * patch: remove UTs from charm repo (#429)
    
    * keep only one simple test
    
    * fix UT
    
    * rmeove all dependencies
    
    * chore: Bump DPW to v35
    
    ---------
    
    Co-authored-by: Patricia Reinoso <patricia.reinoso@canonical.com>

--- tests/unit/helpers.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright 2021 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-from pathlib import Path
-
-import yaml
-
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
