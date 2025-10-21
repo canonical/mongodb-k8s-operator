3e4c6fb60d4ccff53dafc63ac6096df0770dd2a5
Neha Oudin <neha@oudin.red>
2025-10-21 17:57:56 +0200

    feat: Upgrade V3

--- src/charm.py
+++ src/charm.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
-"""Charm code for MongoDB service on Kubernetes."""
-
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
+"""Charm code for MongoDB service on Kubernetes."""
+
+import logging
+
+from ops.log import JujuLogHandler
 from ops.main import main
 from single_kernel_mongo.abstract_charm import AbstractMongoCharm
 from single_kernel_mongo.config.literals import Substrates
 from single_kernel_mongo.config.relations import PeerRelationNames
 from single_kernel_mongo.core.structured_config import MongoDBCharmConfig
 from single_kernel_mongo.managers.mongodb_operator import MongoDBOperator
+
+# Show logger name (module name) in logs
+root_logger = logging.getLogger()
+for handler in root_logger.handlers:
+    if isinstance(handler, JujuLogHandler):
+        handler.setFormatter(logging.Formatter("{name}:{message}", style="{"))
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 
 class MongoDBK8sCharm(AbstractMongoCharm[MongoDBCharmConfig, MongoDBOperator]):
